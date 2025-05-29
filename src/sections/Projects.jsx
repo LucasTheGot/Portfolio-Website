e7aa920c43faba17b3421b171a043bdb04e63a7f
LucasTheGot <lucasthegoatyt@gmail.com>
2025-05-29 13:39:26 -0600

    added images

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -30,11 +30,12 @@ const Projects = () => {
           ))}
         </div>
 
+        {/* YouTube video embed */}
         <div className="flex justify-center mt-12">
           <div className="w-full max-w-3xl aspect-video">
             <iframe
               className="w-full h-full rounded-lg shadow-lg"
-              src="https://youtube.com/embed/UCVMLj6zvOg?si=meOJgaVIvvRvVCqt"
+              src="https://www.youtube.com/embed/UCVMLj6zvOg?si=meOJgaVIvvRvVCqt"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -43,6 +44,17 @@ const Projects = () => {
           </div>
         </div>
 
+        {/* Image grid with 13 photos */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-12">
+          {Array.from({ length: 13 }).map((_, i) => (
+            <img
+              key={i}
+              src={`/images/image${i + 1}.jpg`}
+              alt={`Screenshot ${i + 1}`}
+              className="w-full rounded-lg shadow-md"
+            />
+          ))}
+        </div>
       </div>
     </section>
   )
